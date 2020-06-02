e10a3c37c0659593ac7916e7c3e98900d5134a2b
Devin <devdhera@gmail.com>
2020-06-02 21:31:09 +0530

    Configured post create layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 export default () => {
-    return <div>Blog App</div>;
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
+    );
 };
